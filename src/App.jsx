a982e6b173b2b1464e1b505b79c1ad2d6eddcc0e
Takshil Pandya <takshil.pandya123@gmail.com>
2025-02-19 23:57:19 +0530

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from './sections/navbar.jsx'
+import Hero from './sections/hero.jsx'
+import Ecosystem from './sections/ecosystem.jsx'
+import Pricing from './sections/pricing.jsx'
+import Marketeducations from './sections/marketeducations.jsx'
+import Openacount from './sections/openacount.jsx'
+import Footer from './sections/footer.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Ecosystem/>
+      <Pricing/>
+      <Marketeducations/>
+      <Openacount/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.jsx
@@ -0,0 +1,8 @@
+
+const Button = () => {
+  return (
+    <button className='bg-[#387ED1] text-white font-semibold py-2 px-10 text-[20px] mt-5 hover:bg-black cursor-pointer' >Sign Up for free</button>
+  )
+}
+
+export default Button
